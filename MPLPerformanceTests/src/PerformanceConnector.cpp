3b8ce28fefbf4e56d097ceba49cb5c4c6478782e
Mike Corley <mwcorley79@gmail.com>
2020-08-12 22:26:50 -0400

    add timing to performance tests

--- MPLPerformanceTests/src/PerformanceConnector.cpp
+++ MPLPerformanceTests/src/PerformanceConnector.cpp
@@ -3,25 +3,45 @@
 #include <vector>
 #include <iostream>
 #include <mpl.h>
+#include <chrono>
 
 using namespace CSE384;
+using namespace std::chrono;
 
-void SenderProc(TCPConnector *connector)
+const int MSG_SIZE = 1024;
+
+inline void SyncPrint(const std::string& message)
 {
-   MessagePtr msg;
-   while ((msg = connector->GetMessage())->GetType() != MessageType::DISCONNECT)
-   {
-     // std::cout << msg->ToString() << std::endl;
-   }
+   static std::mutex ioLock;
+   std::lock_guard<std::mutex> l(ioLock);
+   std::cout << message << std::endl;
 }
+
+void ReceiverProc(TCPConnector *connector)
+{
+   SyncPrint("Begin Receiving messages of size " + std::to_string(MSG_SIZE)); 
+   high_resolution_clock::time_point t1 = high_resolution_clock::now();
+   MessagePtr msg;
+   int count = 0;
+   while ((msg = connector->ReceiveMessage())->GetType() != MessageType::DISCONNECT)
+   //while ((msg = connector->GetMessage())->GetType() != MessageType::DISCONNECT)
+   {
+     ++count; // std::cout << msg->ToString() << std::endl;
+   }
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+    SyncPrint("Finished Receiving messages: " + std::to_string(count)  + " of size " + std::to_string(MSG_SIZE)); 
+    SyncPrint("Receive Processing time    : " + std::to_string(time_span.count()) +  " seconds.");
+    SyncPrint("Receive Thoughput          : " + std::to_string(((unsigned long long) count * (MSG_SIZE * 8)) / time_span.count()) +  " bits/sec");
+}
+
+
 
 int main()
 {
    // specify the server Endpoint we wish to connect
    EndPoint serverEp("127.0.0.1", 6060);
    //EndPoint serverEp("::1", 6060);
-
-   const int MSG_SIZE = 1024;
 
    // instantiate a sender
    FixedSizeMsgConnector perfConnector(MSG_SIZE);
@@ -30,20 +50,22 @@ int main()
    int verbose_level = 1;     // verbose (display connect attempts)
    int connect_attempts = 10; // make 10 connection attempts
 
+   perfConnector.UseReceiveQueue(false);
+   perfConnector.UseSendQueue(false);
+   
    if (perfConnector.ConnectPersist(serverEp, connect_attempts, wait_secs, verbose_level) < connect_attempts)
    {
       //handle messages received in separate thread
-      std::thread receiverProc(SenderProc, &perfConnector);
+      std::thread receiverProc(ReceiverProc, &perfConnector);
 
       int num_messages = 1000000;
-      // loop to send 10 messages
+      SyncPrint("Begin Sending: " + std::to_string(num_messages)  + " of size " + std::to_string(MSG_SIZE)); 
+      high_resolution_clock::time_point t1 = high_resolution_clock::now();
+      // loop to send messages
       for (int j = 0; j < num_messages; j++)
       {
-         // build an and print each string message
-         std::string strMsg = "[ Message #: " + std::to_string(j + 1) + " ]";
-       
-         MessagePtr msg = Message::CreateFixedSizeMessage(MSG_SIZE, strMsg);
-        // std::cout << "Message is: " << *msg << std::endl;
+         MessagePtr msg = Message::CreateFixedSizeMessage(MSG_SIZE, "[ Message #: " + std::to_string(j + 1) + " ]");
+         // std::cout << "Message is: " << *msg << std::endl;
 
          // post message into the send queue
          // perfConnector.PostMessage(msg);
@@ -55,8 +77,13 @@ int main()
          // sleep for a second so we can see what's going on
          //std::this_thread::sleep_for(std::chrono::seconds(1));
       }
-
+      high_resolution_clock::time_point t2 = high_resolution_clock::now();
+      duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
       perfConnector.Close();
+      SyncPrint("Finished Sending     : " + std::to_string(num_messages)  + " of size " + std::to_string(MSG_SIZE)); 
+      SyncPrint("Send Processing time : " + std::to_string(time_span.count()) +  " seconds.");
+      SyncPrint("Send Thoughput       : " + std::to_string(((unsigned long long) num_messages * (MSG_SIZE * 8)) / time_span.count()) +  " bits/sec");
+
       receiverProc.join();
    }
    else
--- MPLPerformanceTests/src/PerformanceResponder.cpp
+++ MPLPerformanceTests/src/PerformanceResponder.cpp
@@ -6,12 +6,39 @@
 using namespace CSE384;
 using namespace std::chrono;
 
+const int MSG_SIZE = 1024;
+
+inline void SyncPrint(const std::string &message)
+{
+   static std::mutex ioLock;
+   std::lock_guard<std::mutex> l(ioLock);
+   std::cout << message << std::endl;
+}
+
+void SendProc(FixedSizeMsgClientHander *handler)
+{
+   int num_messages = 1000000;
+   SyncPrint("Begin Sending: " + std::to_string(num_messages) + " of size " + std::to_string(MSG_SIZE));
+   high_resolution_clock::time_point t1 = high_resolution_clock::now();
+   // loop to send messages
+   for (int j = 0; j < num_messages; j++)
+   {
+      handler->SendMessage(Message::CreateFixedSizeMessage(MSG_SIZE, "[ Message #: " + std::to_string(j + 1) + " ]"));
+   }
+   high_resolution_clock::time_point t2 = high_resolution_clock::now();
+   duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+   SyncPrint("Finished Sending     : " + std::to_string(num_messages) + " of size " + std::to_string(MSG_SIZE));
+   SyncPrint("Send Processing time : " + std::to_string(time_span.count()) + " seconds.");
+   SyncPrint("Send Thoughput       : " + std::to_string(((unsigned long long)num_messages * (MSG_SIZE * 8)) / time_span.count()) + " bits/sec");
+}
+
 class ResponderClientHandler : public FixedSizeMsgClientHander
 {
 public:
-   
-   ResponderClientHandler(int msg_size): FixedSizeMsgClientHander(msg_size)
-   {}
+   ResponderClientHandler(int msg_size) : FixedSizeMsgClientHander(msg_size)
+   {
+   }
+
    // this is a creational function: you must implement it
    // the TCPResponder creates an instance of this class (it's how the server polymorphism works)
    virtual ClientHandler *Clone()
@@ -22,29 +49,37 @@ public:
    // this is where you define the custom server processing: you must implement it
    virtual void AppProc()
    {
+      // handle messages sent to TCPConnector (client) in separate thread
+      std::thread senderProc(SendProc, this);
+
       // while there are messages in the blocking queue, and you have seen the disconnect
       // message, pull messages out and dispaly them.
-      
+      int count = 0;
       high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
       MessagePtr msg;
-      while ((msg = GetMessage())->GetType() != MessageType::DISCONNECT)
-      {   
-        // std::cout << "Got a message:" << *msg << "from: " << RemoteEP() << std::endl;
+      while ((msg = ReceiveMessage())->GetType() != MessageType::DISCONNECT)
+      //while ((msg = GetMessage())->GetType() != MessageType::DISCONNECT)
+      {
+         // std::cout << "Got a message:" << *msg << "from: " << RemoteEP() << std::endl;
+         ++count;
          MessagePtr msgPtr = Message::CreateFixedSizeMessage(GetMessageSize(),
-                         std::string("Reply from server: ") + GetServiceEndPoint().ToString() 
-         );
+                                                             std::string("Reply from server: ") + GetServiceEndPoint().ToString());
 
          // queue reply message
          // PostMessage(msgPtr);
 
          // send reply message directly (no queue)
-         SendMessage(msgPtr);
+         //SendMessage(msgPtr);
       }
 
       high_resolution_clock::time_point t2 = high_resolution_clock::now();
       duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-      std::cout << "Processing time:" << time_span.count() << " seconds." << std::endl;
+      SyncPrint("Finished Receiving messages: " + std::to_string(count) + " of size " + std::to_string(MSG_SIZE));
+      SyncPrint("Receive Processing time    : " + std::to_string(time_span.count()) + " seconds.");
+      SyncPrint("Receive Thoughput          : " + std::to_string(((unsigned long long)count * (MSG_SIZE * 8)) / time_span.count()) + " bits/sec");
+
+      senderProc.join();
    }
 
    ~ResponderClientHandler()
@@ -59,8 +94,6 @@ int main(int argc, char *argv[])
    EndPoint serverEP("127.0.0.1", 6060);
    //EndPoint serverEP("::1", 6060);
 
-   const int MSG_SIZE = 1024;
-   
    ResponderClientHandler rh(MSG_SIZE);
 
    // set TCP socket options
@@ -70,6 +103,9 @@ int main(int argc, char *argv[])
    // define instance of the TCPResponder (server host)
    TCPResponder responder(serverEP, &sock_opts);
 
+   responder.UseClientSendQueue(false);
+   responder.UseClientReceiveQueue(false);
+
    // register the custom client handler with TCPResponder instance
    responder.RegisterClientHandler(&rh);
 
@@ -77,7 +113,7 @@ int main(int argc, char *argv[])
    responder.Start();
    std::cout << "Server listening on: " << serverEP << std::endl;
    std::cout << "Press any key to quit" << std::endl;
-   ::getchar();
+   std::cin.get();
 
    // stop the listener and quit
    responder.Stop();
